34c52d11d6fa3215eb2427bca5574409798465aa
nascimbem0712 <joaonascimbem0712@gmail.com>
2024-12-05 22:47:58 -0300

    Update controlehabito.js

--- controlehabito.js
+++ controlehabito.js
@@ -1,1 +1,12 @@
-//
+git checkout -b ui-design
+
+git checkout -b backend-logic
+
+git checkout -b progress-tracking
+
+git checkout ui-design
+
+git add .
+git commit -m "Implementa layout inicial da interface"
+git checkout main
+git merge nome-da-branch
